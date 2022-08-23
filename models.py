7e78b9ebf6de58a1628e882b7d2cd4d0e5f289ae
adekilekunabdullahi <106445363+adekilekunabdullahi@users.noreply.github.com>
2022-08-23 06:39:52 +0100

    Update models.py

--- models.py
+++ models.py
@@ -28,6 +28,7 @@ class Venue(db.Model):
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default=True)
     seeking_description = db.Column(db.String())
+    Show = db.relationship('Show', backref='venue', lazy='joined')
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
@@ -44,15 +45,15 @@ class Artist(db.Model):
     website = db.Column(db.String())
     seeking_venue = db.Column(db.Boolean, default=True)
     seeking_description= db.Column(db.String(200))
-    Show = db.relationship('Venue', secondary='Show', backref=db.backref('artist', lazy='joined'))
+    Show = db.relationship('Show', backref='artist', lazy='joined')
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
     __tablename__ = 'Show'
     id = db.Column(db.Integer, primary_key=True)
-    artist_name = db.Column( db.Integer, db.ForeignKey('Artist.name'), nullable=True)
-    venue_name = db.Column(db.Integer, db.ForeignKey('Venue.name'), nullable=True)
+    artist_id = db.Column( db.Integer, db.ForeignKey('Artist.id'), nullable=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=True)
     start_time = db.Column(db.DateTime)
 
 
